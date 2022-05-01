3e6c051ee8cd68f6f24ef3e6a927c5e7755cd18c
Shriya <shriya.atmak@gmail.com>
2022-05-01 17:53:13 -0400

    Added document chunking for QA Dataset

--- dataset/qa_dataset.py
+++ dataset/qa_dataset.py
@@ -1,42 +1,87 @@
-from torch.utils.data import Dataset, DataLoader
-from datasets import load_dataset
-from transformers import AutoTokenizer
+import torch
+from torch.utils.data import DataLoader
 from quac_dataset import QuACDataset
 from easydict import EasyDict
 
 class QuACQADataset(QuACDataset):
     def __init__(self, config, split):
         super().__init__(config, split)
-        self.num_prev_turns = int(self.config.num_prev_turns)
+        self.chunked_data = self.chunk_data()
 
-    def __getitem__(self, idx):
-        item = self.flattened_data[idx]
+    def chunk_data(self):
+        chunked_data = []
+        for item in self.flattened_data:
+            all_tokens = self.tokenizer(item['context'])
+            
+            answer_start = item['orig_answer']['answer_start'] if not item['context'][item['orig_answer']['answer_start']].isspace() else item['orig_answer']['answer_start'] - 1
+            answer_end = item['orig_answer']['answer_end'] if not item['context'][item['orig_answer']['answer_end']].isspace() else item['orig_answer']['answer_end'] - 1
 
-        turn_no = int(item['turn_id'].split('#')[-1])
-        turn_history = ""
-        if turn_no > 0:
-            for i in range(max(turn_no-self.num_prev_turns, 0), turn_no):
-                try:
-                    history_turn_id = item['turn_id'].split('#')[0] + '#' + str(i)
-                    history_idx = self.turn_id_idx_map[history_turn_id]
-                    turn_history += " " + self.flattened_data[history_idx]['question']
-                    turn_history += " " + self.flattened_data[history_idx]['orig_answer']['text']
-                except KeyError:
-                    pass
-        question_with_history = (turn_history.strip() + " " + item['question']).strip()
-
-        inputs = self.tokenizer(question_with_history, item['context'], truncation='longest_first', padding='max_length', max_length=self.config.max_len, return_tensors='pt')
+            start_position, end_position = all_tokens.char_to_token(answer_start), all_tokens.char_to_token(answer_end)
+            chunked_item, chunk_len = self.chunk_passage(item)
         
-        start_token = inputs.char_to_token(item['orig_answer']['answer_start'], sequence_index=1)
-        if start_token is None:
-            start_token = self.config.max_len - 1
-        end_token = inputs.char_to_token(item['orig_answer']['answer_end'], sequence_index=1)
-        if end_token is None:
-            end_token = self.config.max_len - 1
+            for chunk in chunked_item:
+                chunk_dict = {}
 
-        return {'input_ids': inputs['input_ids'], 'attention_mask': inputs['attention_mask'], 'start_positions': start_token, 'end_positions': end_token, 'qid': item['turn_id'], 'yesno': item['yesno'], 'followup': item['followup']}
+                for k in item.keys():
+                    chunk_dict[k] = item[k]
+
+                chunk_dict['input_ids'] = chunk['input_ids']
+                chunk_dict['attention_mask'] = chunk['attention_mask']
+
+                if chunk['start_offset'] + chunk_len > start_position and chunk['start_offset'] < end_position:
+                    if chunk['start_offset'] <= start_position:
+                        chunk_dict['start_position'] = start_position - chunk['start_offset']
+                    else:
+                        chunk_dict['start_position'] = 0
+                    if chunk['start_offset'] + chunk_len >= end_position:
+                        chunk_dict['end_position'] = end_position - chunk['start_offset']
+                    else:
+                        chunk_dict['end_position'] = chunk_len - 1
+                    chunked_data.append(chunk_dict)
+
+                elif self.split != 'train':
+                    chunk_dict['start_position'] = self.config.max_len
+                    chunk_dict['end_position'] = self.config.max_len
+                    chunked_data.append(chunk_dict)
+
+        return chunked_data
+
+    def chunk_passage(self, item):
+        max_query_len = self.config.max_query_len
+        question_with_history = self.get_history_turns(item)
+        max_seq_length = self.config.max_len
+        
+        tokenized_question = self.tokenizer(question_with_history, max_length=max_query_len, truncation=True, padding=False)
+        query_len = min(len(tokenized_question['input_ids']), max_query_len)
+        context_len = max_seq_length - query_len - 1 # -1 for final [SEP] token
+
+        context_tokenized = self.tokenizer(item['context'], max_length=context_len, truncation=True, padding=False, return_overflowing_tokens=True, add_special_tokens=False, stride=self.config.context_stride)
+        chunked_items = []
+        start_offset = 0
+
+        for i in range(len(context_tokenized['input_ids'])):
+            input_ids = tokenized_question['input_ids'] + context_tokenized['input_ids'][i] + [self.tokenizer.convert_tokens_to_ids('[SEP]')]
+            attention_mask = tokenized_question['attention_mask'] + context_tokenized['attention_mask'][i] + [1]
+            
+            if i == len(context_tokenized['input_ids']) - 1:
+                input_ids = input_ids + ([self.tokenizer.convert_tokens_to_ids('[PAD]')] * (max_seq_length-len(input_ids)))
+                attention_mask = attention_mask + ([0] * (max_seq_length-len(attention_mask)))
+
+            input_ids = torch.tensor(input_ids)
+            attention_mask = torch.tensor(attention_mask)
+
+            chunked_items.append({'input_ids':input_ids, 'attention_mask':attention_mask, 'start_offset':start_offset})
+            start_offset += context_len
+        return chunked_items, context_len
+
+    def __len__(self):
+        return len(self.chunked_data)
+        
+    def __getitem__(self, idx):
+        item = self.chunked_data[idx]
+        return {'input_ids': item['input_ids'], 'attention_mask': item['attention_mask'], 'start_positions': item['start_position'], 'end_positions': item['end_position'], 'qid': item['turn_id'], 'yesno': item['yesno'], 'followup': item['followup']}
 
 if __name__=='__main__':
-    data = QuACQADataset(EasyDict({'model_name': 'distilbert-base-uncased', 'num_prev_turns': '2', 'max_len': 256}), 'train')
+    data = QuACQADataset(EasyDict({'model_name': 'distilbert-base-uncased', 'num_prev_turns': '2', 'max_len': 256, 'max_query_len': 50}), 'train')
     l = len(data)
     sample = data[25]
--- dataset/quac_dataset.py
+++ dataset/quac_dataset.py
@@ -7,9 +7,11 @@ class QuACDataset(Dataset):
     def __init__(self, config, split):
         
         self.config = config
-        self.data = load_dataset('quac', split=split)
+        self.split = split
+        self.data = load_dataset('quac', split=self.split)
         self.flattened_data, self.turn_id_idx_map = self.flatten_data()
         self.tokenizer = AutoTokenizer.from_pretrained(self.config.model_name, use_fast=True)
+        self.num_prev_turns = int(self.config.num_prev_turns)
 
     def flatten_data(self):
         flattened_data = []
@@ -24,14 +26,28 @@ class QuACDataset(Dataset):
                     row_dict[k] = row[k]
                 for k in multi_keys:
                     row_dict[k[:-1]] = row[k][i]
-                row_dict['orig_answer'] = {'answer_start': int(row['orig_answers']['answer_starts'][i]), 'answer_end': int(row['orig_answers']['answer_starts'][i]) + len(row['orig_answers']['texts'][i]), 'text':row['orig_answers']['texts'][i]}
+                row_dict['orig_answer'] = {'answer_start': int(row['orig_answers']['answer_starts'][i]), 'answer_end': int(row['orig_answers']['answer_starts'][i]) + len(row['orig_answers']['texts'][i]) - 1, 'text':row['orig_answers']['texts'][i]}
                 row_dict['answers'] = {'answer_starts': [int(x) for x in row['answers']['answer_starts'][i]], 'texts': row['answers']['texts'][i]}
-                row_dict['answer_ends'] = [x + len(row_dict['answers']['texts'][j]) for j,x in enumerate(row_dict['answers']['answer_starts'])]
+                row_dict['answer_ends'] = [x - 1 + len(row_dict['answers']['texts'][j]) for j,x in enumerate(row_dict['answers']['answer_starts'])]
                 flattened_data.append(row_dict)
                 turn_id_idx_map[row_dict['turn_id']] = idx
                 idx += 1
         return flattened_data, turn_id_idx_map
-        
+
+    def get_history_turns(self, item):
+        turn_no = int(item['turn_id'].split('#')[-1])
+        turn_history = ""
+        if turn_no > 0:
+            for i in range(max(turn_no-self.num_prev_turns, 0), turn_no):
+                try:
+                    history_turn_id = item['turn_id'].split('#')[0] + '#' + str(i)
+                    history_idx = self.turn_id_idx_map[history_turn_id]
+                    turn_history += " " + self.flattened_data[history_idx]['question']
+                    turn_history += " " + self.flattened_data[history_idx]['orig_answer']['text']
+                except KeyError:
+                    pass
+        question_with_history = (turn_history.strip() + " " + item['question']).strip()
+        return question_with_history
 
     def __len__(self):
         return len(self.flattened_data)
